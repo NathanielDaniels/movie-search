c80807c7ca13568ab6e8180326ff359e68e5c988
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-12-29 16:18:37 -0600

    figuring out commit issues

--- src/searchMovies.js
+++ src/searchMovies.js
@@ -3,16 +3,12 @@ import config from '../config'
 
 export default function SearchMovies(){
     
-    //states- input query, movies
     const [query, setQuery] = useState('');
-    //create the state for movies, and update that sts
     const [movies, setMovies] = useState([]);
     
     const searchMovies = async (e) => {
         e.preventDefault();
-
-        const mykey = config.MY_KEY;
-                
+        
         const url = `https://api.themoviedb.org/3/search/movie?api_key=5552fffb8a95ca814aa5e4b9d3a3c335&language=en-US&query=${query}&page=1&include_adult=false`;
         
         try {
